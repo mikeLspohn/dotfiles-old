4cbdcf5dd51e2231a441868d05891d3a8cd3c5e0
Michael Spohn <michael@Michaels-MacBook-Pro.local>
2014-10-24 19:10:18 -0500

    Updated install

--- symlinks.sh
+++ symlinks.sh
@@ -6,7 +6,7 @@ h
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
+files="bashrc tmux.conf vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
 
 ##########
 
