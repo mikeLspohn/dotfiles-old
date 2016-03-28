952a23f630ac5551a14acf6ef0faee6d72699453
Michael Spohn <mikeLspohn@gmail.com>
2016-03-27 21:59:39 -0500

    Fix syntax error for installing oh-my-zsh

--- symlinks.sh
+++ symlinks.sh
@@ -36,13 +36,14 @@ install_zsh () {
 # Test to see if zshell is installed.  If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
+    if [ ! -d $dir/oh-my-zsh/ ]; then
         echo "Installing oh-my-zh"
         git clone http://github.com/robbyrussell/oh-my-zsh.git
+        echo "Creating default .zshrc"
         cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
     fi
     # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+    if [ ! $(echo $SHELL) == $(which zsh) ]; then
         echo "Setting ZSH as default shell!"
         chsh -s $(which zsh)
     else
